38d7d89902174c1451448041f7a1563ae73d3fa9
Jj Kwan <tringpore@gmail.com>
2023-10-26 23:59:20 +0800

    updated base ports

--- include/globals.hpp
+++ include/globals.hpp
@@ -2,13 +2,14 @@
 #define _GLOBALS_HPP_
 
 //base
-#define lfb_port 2
-#define lft_port 3
-#define lbb_port 11
-#define lbt_port 15
-#define rfb_port 9
-#define rft_port 10
-#define rbb_port 19
-#define rbt_port 20
+#define lfb_port 1
+#define lft_port 2
+#define lbb_port 10
+#define lbt_port 9
+#define rfb_port 16
+#define rft_port 17
+#define rbb_port 18
+#define rbt_port 19
+//port 20 clip on brain broken
 
 #endif
